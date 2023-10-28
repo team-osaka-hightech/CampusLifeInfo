cc488f089a2680314393b97df3084295e3f66781
sousci <soudai.usui@mineo.jp>
2023-10-28 12:33:42 +0900

    Add 1week,2week display mode.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let displayMode = '1week';
+
 async function loadTimetable() {
   try {
     const response = await fetch('https://raw.githubusercontent.com/team-osaka-hightech/CampusLifeInfo/main/2023s_timetable.json');
@@ -51,6 +53,50 @@ async function loadTimetable() {
   } catch (error) {
     console.error('Error loading timetable:', error);
   }
+
+  setDisplayMode(displayMode);  // 初期表示設定
+}
+
+function setDisplayMode(mode) {
+  displayMode = mode;
+  const container = document.getElementById('timetable-container');
+  const tables = container.querySelectorAll('table');
+  const today = new Date();
+  const startOfWeek = today.getDate() - today.getDay() + (today.getDay() === 0 ? -6 : 1);
+  const endOfWeek = startOfWeek + 6;
+
+  tables.forEach(table => {
+    const dateHeader = table.previousSibling;
+    const dateMatch = dateHeader.textContent.match(/(\d{4})\/(\d{2})\/(\d{2})/);
+    const tableDate = new Date(dateMatch[1], dateMatch[2] - 1, dateMatch[3]);
+
+    switch (mode) {
+      case '1week':
+        if (tableDate >= new Date(today.getFullYear(), today.getMonth(), startOfWeek) &&
+            tableDate <= new Date(today.getFullYear(), today.getMonth(), endOfWeek)) {
+          table.style.display = '';
+          dateHeader.style.display = '';
+        } else {
+          table.style.display = 'none';
+          dateHeader.style.display = 'none';
+        }
+        break;
+      case '2week':
+        if (tableDate >= new Date(today.getFullYear(), today.getMonth(), startOfWeek) &&
+            tableDate <= new Date(today.getFullYear(), today.getMonth(), endOfWeek + 7)) {
+          table.style.display = '';
+          dateHeader.style.display = '';
+        } else {
+          table.style.display = 'none';
+          dateHeader.style.display = 'none';
+        }
+        break;
+      case 'all':
+        table.style.display = '';
+        dateHeader.style.display = '';
+        break;
+      }
+  });
 }
 
 loadTimetable();
